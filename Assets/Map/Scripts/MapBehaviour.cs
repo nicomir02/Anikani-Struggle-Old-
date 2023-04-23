a54c9b3d2a00cd4761151ec2983f4775189a8954
Nico Mirchandani <2002nicomir@gmail.com>
2023-04-24 00:32:18 +0200

    Neues Script für Map
    Übersichtlicher, einfacher Abrufbar, aber funktioniert genau gleich

--- Assets/Map/Scripts/MapBehaviour.cs
+++ Assets/Map/Scripts/MapBehaviour.cs
@@ -7,88 +7,63 @@ using UnityEngine.Tilemaps;
 
 public class MapBehaviour : NetworkBehaviour
 {
+    //BlockDetails
+    public struct BlockDetails {
+        public int Biomindex;
+        public int Blockindex;
+        public bool Ressourcenbool;
+        public int ressindex;
+
+        public BlockDetails(int biom, int block) {
+            Biomindex = biom;
+            Blockindex = block;
+            Ressourcenbool = false;
+            ressindex = 0;
+        }
+
+        public BlockDetails(int biom, int block, int ress) {
+            Biomindex = biom;
+            Blockindex = block;
+            Ressourcenbool = true;
+            ressindex = ress;
+        }
+    }
+
     [SerializeField] private Biom[] biome;
-    [SerializeField] private Ressource[] ressource;
+    [SerializeField] private Ressource[] ressourcen;
 
-    private readonly SyncList<Vector3Int> vectorsave = new SyncList<Vector3Int>();
-    
-    private readonly SyncList<int> biomindex = new SyncList<int>();
-    private readonly SyncList<int> blockindex = new SyncList<int>();
-    //^^oberhalb Cantorsche Paarungsfunktion einrichten?^^
+    //SyncDictionary<Vector3Int, (Biomindex, Blockindex, Ressourcenbool, ressindex)> eindeutiger als vorher
+    public readonly SyncDictionary<Vector3Int, BlockDetails> blockDetails = new SyncDictionary<Vector3Int, BlockDetails>();
 
-    private readonly SyncList<Vector3Int> ressourcevec = new SyncList<Vector3Int>();
-    private readonly SyncList<int> ressindex = new SyncList<int>();
+    //Random
+    private System.Random rand = new System.Random();
 
-    HashSet<Vector3Int> vectors = new HashSet<Vector3Int>();
-    private Dictionary<Vector3Int, Biom> alleBiome = new Dictionary<Vector3Int, Biom>();
-
-    private System.Random rand = new System.Random(); //Für Zufallsvariablen
-
-    [SerializeField] private int width = 25;
+    //Einstellungen Größe der Karte
+    [SerializeField] private int width = 25; 
     [SerializeField] private int height = 25;
     [SerializeField] private int biomgroesse = 250;
 
+    //Karteneinstellungen
     [SerializeField] private Tilemap tilemap;
-
     [SerializeField] private TileBase randTile;
 
-    public (Biom, Block, Ressource) getBlockDetails(Vector3Int vec) {
-        Biom biom = null;
-        Block block = null;
-        Ressource ress = null;
-
-        biom = getBiomByVec(vec);
-        if(ressourcevec.Contains(vec)) {
-            for(int i=0; i<ressourcevec.Count; i++) {
-                if(ressourcevec[i] == vec) {
-                    ress = ressource[ressindex[i]];
-                    block = ress.getBlock();
-                    break;
-                }
-            }
-        }else {
-            for(int i=0; i<vectorsave.Count; i++) {
-                if(vectorsave[i] == vec) {
-                    block = biom.getBlockByIndex(blockindex[i]);
-                }
-            }
-        }
-        
-        return (biom, block, ress);
-    }
-
-    public int mapWidth() {
-        return width;
-    }
-
-    public int mapHeight() {
-        return height;
-    }
+    private HashSet<Vector3Int> vectors = new HashSet<Vector3Int>();
 
     void Start() {
         if(isServer) {
             createTerrain();
         }else {
-            buildTerrain();
+            //buildTerrain();
             //Debug.Log(getBlockDetails(new Vector3Int(0,0,0)));
         }
     }
 
     void createTerrain() {
-        for(int x=-1; x<=1; x++) {
-            for(int y=-1; y<=1; y++) {
-                tilemap.SetTile(new Vector3Int(x, y, 0), null);
-            }
-        }
-
-        for(int x=0; x<=width; x++) {
-            for(int y=0; y<=height; y++) {
-                vectors.Add(new Vector3Int(x, y, 0));
-            }
-        }
+        resetAll();
 
         int i=0;
         float biomes = (width*height)/biomgroesse*1.1f; 
+
         while(vectors.Count > 0 || i <= Math.Ceiling(biomes)) {
             createBiom();
             i++;
@@ -101,98 +76,33 @@ public class MapBehaviour : NetworkBehaviour
                 Biom biom = getMostNeighbors(vect);
                 int indexblock = rand.Next(biom.countBlocks());
                 tilemap.SetTile(vect, biom.getBlockByIndex(indexblock).getTile());
-                alleBiome[vect] = biom;
                 int indexbiom = 0;
+
                 for(i=0; i<biome.Length; i++) {
                     if(biome[i] == biom) {
                         indexbiom=0;
                         break;
                     }
                 }
-                saveBlock(vect, indexbiom, indexblock);
-            }
-        }
 
-        for(int x=0; x<=width; x++) {
-            for(int y=0; y<=height; y++) {
-                vectors.Add(new Vector3Int(x, y, 0));
-            }
-        }
-
-        foreach(Vector3Int vec in vectors) {
-            int x = rand.Next(ressource.Length);
-            Ressource ress = ressource[x];
-
-            Biom curBiom = alleBiome[vec];
-
-            int max = 10000;
-            
-            if(rand.Next(0, max) <= (ress.getProb(curBiom)*max)) {
-                tilemap.SetTile(vec, ress.getRessourceTile());
-                ressourcevec.Add(vec);
-                ressindex.Add(x);
+                blockDetails.Add(vect, new BlockDetails(indexbiom, indexblock));
             }
         }
 
         createRand();
     }
 
-    void createRand() {
-        int a=height+1;
-
-        for(int i=-1; i<=a; i++) {
-            Vector3Int vec = new Vector3Int(i, a, 0);
-            tilemap.SetTile(vec, randTile);
-            vec = new Vector3Int(i, -1, 0);
-            tilemap.SetTile(vec, randTile);
-        }
-
-        for(int i=-1; i<=a; i++) {
-            Vector3Int vec = new Vector3Int(a, i, 0);
-            tilemap.SetTile(vec, randTile);
-            vec = new Vector3Int(-1, i, 0);
-            tilemap.SetTile(vec, randTile);
-        }
-    }
-
-    Biom getMostNeighbors(Vector3Int vec) {
-        Dictionary<Biom, int> biomCounter = new Dictionary<Biom, int>();
-        Biom curBiom = null;
-
-        for(int x=-1; x<=1; x++) {
-            for(int y=-1; y<=1; y++) {
-                vec = new Vector3Int(x, y, 0) + vec;
-                if(alleBiome.ContainsKey(vec)) {
-                    curBiom = alleBiome[vec];
-                    if(biomCounter.ContainsKey(curBiom)) {
-                        biomCounter[curBiom]++;
-                    }else {
-                        biomCounter.Add(curBiom, 1);
-                    }
-                }
-            }
-        }
-
-        if(biomCounter.Count > 0) {
-            int i=0;
-            foreach (KeyValuePair<Biom, int> pair in biomCounter) { 
-                if(pair.Value > i) {
-                    curBiom = pair.Key;
-                    i = pair.Value;
-                }
-            }
-            return curBiom;
-        }else {
-            return null;
-        }
+    public Biom getBiomByVec(Vector3Int vec) {
+        return biome[blockDetails[vec].Blockindex];
     }
 
     void createBiom() {
-        List<Vector3Int> neighbors = new List<Vector3Int>();
-        Vector3Int vec = rndmValidVec();
-        
+        List<Vector3Int> neighbors = new List<Vector3Int>(); //Verfügbare Nachbarn Speicher
+        Vector3Int vec = rndmValidVec(); //Anfangsvektor
+
         int indexbiom = rand.Next(biome.Length);
         Biom b = biome[indexbiom];
+
         if(getMostNeighbors(vec) != null) {
             b = getMostNeighbors(vec);
             int i=0;
@@ -207,30 +117,52 @@ public class MapBehaviour : NetworkBehaviour
 
         int indexblock = rand.Next(b.countBlocks());
         Block rndm = b.getBlockByIndex(indexblock);
+    	
         tilemap.SetTile(vec, rndm.getTile());
+
         neighbors.AddRange(getValidNeighbors(vec));
 
-        saveBlock(vec, indexbiom, indexblock);
-        alleBiome.Add(vec, b);
+        vectors.Remove(vec);
+
+        blockDetails.Add(vec, new BlockDetails(indexbiom, indexblock));
 
         for(int i=0; i<=biomgroesse*b.getBiomMultiplier(); i++) {
             if(neighbors.Count > 0) {
                 vec = neighbors[rand.Next(neighbors.Count)];
-                if(!alleBiome.ContainsKey(vec)) {
+                if(!blockDetails.ContainsKey(vec)) {
                     indexblock = rand.Next(b.countBlocks());
                     rndm = b.getBlockByIndex(indexblock);
 
-                    saveBlock(vec, indexbiom, indexblock);
+                    blockDetails.Add(vec, new BlockDetails(indexbiom, indexblock));
 
                     tilemap.SetTile(vec, rndm.getTile());
-
+                    vectors.Remove(vec);
                     neighbors.AddRange(getValidNeighbors(vec));
                     neighbors = updateNeighbors(neighbors);
-                    alleBiome.Add(vec, b);
                 }
             }else {
                 break;
             }
+        }
+
+    }
+
+    void createRand() {
+        int a=height+1;
+
+        for(int i=-1; i<=a; i++) {
+            Vector3Int vec = new Vector3Int(i, a, 0);
+            tilemap.SetTile(vec, randTile);
+            vec = new Vector3Int(i, -1, 0);
+            tilemap.SetTile(vec, randTile);
+        }
+
+        a = width+1;
+        for(int i=-1; i<=a; i++) {
+            Vector3Int vec = new Vector3Int(a, i, 0);
+            tilemap.SetTile(vec, randTile);
+            vec = new Vector3Int(-1, i, 0);
+            tilemap.SetTile(vec, randTile);
         }
     }
 
@@ -259,54 +191,75 @@ public class MapBehaviour : NetworkBehaviour
         return neighbors;
 
     }
+    
+    Biom getMostNeighbors(Vector3Int vec) {
+        Dictionary<Biom, int> biomCounter = new Dictionary<Biom, int>();
+        Biom curBiom = null;
 
-    void buildTerrain() {
-        for(int x=0; x<vectorsave.Count; x++) {
-            tilemap.SetTile(vectorsave[x], getTile(x));
+        for(int x=-1; x<=1; x++) {
+            for(int y=-1; y<=1; y++) {
+                Vector3Int vector = new Vector3Int(x, y, 0) + vec;
+                if(blockDetails.ContainsKey(vector)) {
+                    curBiom = getBiomByVec(vector);
+                    if(biomCounter.ContainsKey(curBiom)) {
+                        biomCounter[curBiom]++;
+                    }else {
+                        biomCounter.Add(curBiom, 1);
+                    }
+                }
+            }
         }
 
-        int i = 0;
-        foreach(Vector3Int vec in ressourcevec) {
-            tilemap.SetTile(vec, ressource[ressindex[i]].getRessourceTile());
-            i++;
+        if(biomCounter.Count > 0) {
+            int i=0;
+            foreach (KeyValuePair<Biom, int> pair in biomCounter) { 
+                if(pair.Value > i) {
+                    curBiom = pair.Key;
+                    i = pair.Value;
+                }
+            }
+            return curBiom;
         }
-
-        createRand();
+        return null;
     }
 
     Vector3Int rndmValidVec() {
-        List<Vector3Int> list = new List<Vector3Int>(vectors);
-        Vector3Int vec = list[rand.Next(vectors.Count)];
-        return vec;
+        List<Vector3Int> vecs = new List<Vector3Int>(vectors);
+        if(vecs.Count > 0) {
+            return vecs[rand.Next(vectors.Count)];
+        }
+        return new Vector3Int(width*width*height*height, width*width*height*height, 0);
     }
 
-    void saveBlock(Vector3Int vec, int indexbiom, int indexblock) {
-        vectors.Remove(vec);
-        vectorsave.Add(vec);
-        blockindex.Add(indexblock);
-        biomindex.Add(indexbiom);
-    }
-
-    Biom getBiomByVec(Vector3Int vec) {
-        if(vectorsave.Contains(vec)) {
-            int i=0;
-            foreach(Vector3Int v in vectorsave) {
-                if(v == vec) {
-                    break;
-                }
-                i++;
+    private void resetAll() {
+        Vector3Int vec = new Vector3Int(0, 0, 0);
+        for(int x=-1; x<=width; x++) {
+            for(int y=-1; y<=height; y++) {
+                //for(int z=-1; z<=1; y++) {
+                vec = new Vector3Int(x, y, 0);
+                tilemap.SetTile(vec, null);
+                vectors.Add(vec);
+                //}
             }
-            return biome[biomindex[i]];
-        }else {
-            return null;
         }
     }
 
-    Block getBlock(int index) {
-        return biome[biomindex[index]].getBlockByIndex(index);
+    public (Biom, Block, Ressource) getBlockDetails(Vector3Int vec) {
+        Biom biom = biome[blockDetails[vec].Biomindex];
+        Block block = biom.getBlockByIndex(blockDetails[vec].Blockindex);
+        Ressource ressource = null;
+        if(blockDetails[vec].Ressourcenbool) {
+            ressource = ressourcen[blockDetails[vec].ressindex];
+        }
+
+        return (biom, block, ressource);
     }
 
-    TileBase getTile(int index) {
-        return getBlock(index).getTile();
+    public int mapHeight() {
+        return height;
+    }
+
+    public int mapWidth() {
+        return width;
     }
 }
--- Assets/Unit/Unit.cs
+++ Assets/Unit/Unit.cs
@@ -6,7 +6,7 @@ using UnityEngine.Tilemaps;
 
 public class Unit : NetworkBehaviour
 {
-    [SyncVar] private int leben = 100;
+    /*[SyncVar] private int leben = 100;
     [SyncVar] private int nahkampf = 10;
     [SyncVar] private int fernkampf = 5;
     [SyncVar] private int fernkampfweite = 2;
@@ -16,15 +16,15 @@ public class Unit : NetworkBehaviour
     [SerializeField] private TileBase truppenTile;
 
     private Tilemap tilemap;
-/*
+
     void Start() {
         tilemap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
     }
-*/
     void truppeSetzen(Vector3Int vec) {
         vec.z = 1;
 
         //tilemap.SetTile(vec, truppenTile);
     }
 
+    */
 }
