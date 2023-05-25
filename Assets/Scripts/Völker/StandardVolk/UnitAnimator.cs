9030adc55e1a3c760a7518799bd10647b5b13d3f
0xKatharina <130848315+0xKatharina@users.noreply.github.com>
2023-05-25 22:47:51 +0200

    er bewegt sich jetzt
    in hübscher geschwindigkeit aber das mit dem drehen geht noch nicht so ganz

--- Assets/Scripts/Völker/StandardVolk/UnitAnimator.cs
+++ Assets/Scripts/Völker/StandardVolk/UnitAnimator.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class UnitAnimator : MonoBehaviour
 {
+    public Sprite[] idle;
     public Sprite[] moveForwardL;
+    //public Sprite[] moveForwardR;
+    public Sprite[] moveBackL;
+    //public Sprite[] moveBackR;
+
     private SpriteRenderer spriteRenderer; 
 
     // Start is called before the first frame update
@@ -16,10 +21,11 @@ public class UnitAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (moveForwardL.Length > 0)
-        {
-            int index = Mathf.FloorToInt(Time.time * 10) % moveForwardL.Length;
-            spriteRenderer.sprite = moveForwardL[index];
-        }
+        int index = Mathf.FloorToInt(Time.time * 4) % moveForwardL.Length;
+        spriteRenderer.sprite = moveForwardL[index];
+    }
+
+    void rumdrehen() {
+        GetComponent<Transform>().Rotate(new Vector3(0f, 180f, 0f));
     }
 }
