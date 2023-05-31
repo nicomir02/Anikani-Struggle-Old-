5926ea81aa48ceb175a5a3a86600fdf2fa157438
0xKatharina <130848315+0xKatharina@users.noreply.github.com>
2023-05-31 21:53:28 +0200

    Bugliste angepasst

--- Assets/BugListe.cs
+++ Assets/BugListe.cs
@@ -5,53 +5,37 @@
     //Beim ausscheiden von einem Spieler werden Units nicht despawned
     //letzter verbleibender Spieler bekommt nicht Win Screen angezeigt
     //man soll sich nicht auf felder stellen können wo Bäume drauf sind oder stein(geändert aber testen)
-    //grobe skizzen/sprites für verschiedene Völker um mal ein umfangreicheres spiel aufzubauen
+    //grobe skizzen/sprites für verschiedene Völker um mal ein umfangreicheres spiel aufzubauen 
+    //wanderer programmierung dahinter
 
 //Mittlere Priorität
 
+    //Sounds
+    //engel bewegungsanimation
     //Code kommentieren [Alex, Nico]
-    //wanderer programmierung dahinter
+    //nicht durch gebäude gehen (beim gebäude bau werden tiles ersetzt durch nicht-walkable tiles)
+    //Name nur von Host, nicht von clients
+
+    //Kostenüberlegungen für Truppen und Gebäude
+    //angegriffene truppen sollen sich im Nahkampf wehren!!
+    //Truppen brauchen mehr Statistiken als Angriff + leben + bewegungsreichweite + angriffsreichweite
+    //generischer flächenschaden bei angriff(für heilung, angriffe, angriffe über zeit(curse, poison))
 
 //Niedrige Priorität
 
-    //Name nur von Host, icht von clients
     //showarea nicht immer geupdated(hauptgebäude setzen, spieler ausgeschieden)
-    //Sounds
-    //nicht durch gebäude gehen (beim gebäude bau werden tiles ersetzt durch nicht-walkable tiles)
     //Angriff schräg 1 Block links 1 block rechts = 2 blöcke deshalb geht da kein angriff, ändern?
     //mindest Spieler anzahl auf 2 setzen damit man nicht alleine auf ready drücken kann(später, da zum testen noch hilfreich)
-    //Truppen brauchen mehr Statistiken als Angriff + leben + bewegungsreichweite + angriffsreichweite
-    //Truppenrekrutierungskosten nicht nur Holz
-    //Namenseingabe in Lobby
+
     //Menü mit existierenden Spielern
     //disqualifyPlayer methode noch im Building Manager Skript(ändern)
     //Methoden in Hilfsskripte und so umlagern und allgemein Skripte säubern
     //sehr lange latency/wartezeit auf seite des clients bei online-modus über hamachi(bleibt laut dozent)
     //manchmal verschwinden Einheiten auf Feldern(im multiplayer)
     //beim unitprefab im unit script sind die farbsprites unnötig da sie im unitanimator skript zu finden sind
-    //ressorucen kosten generisch machen und auch manche sachen andere ressourcen ksoten lassen statt nur mit Holz wie bisher.
-    //verschiedene Farben für alle Einheiten
-    //idle position für verschiedene Farben
     //heilung des engels funktioniert nicht (geheilte truppe nicht mehr anklickbar)
-    //engel bewegungsanimation
-    //max-Bewegung durch pathfinding(noch nicht ausprobiert) z.b. über ein feld wasser geht man aussenrum 6 schritte
-    //generischer flächenschaden bei angriff(für heilung, angriffe, angriffe über zeit(curse, poison))
-    //cheats
     //hauptgebäude soll auch angreifen
-    //angegriffene truppen wehren sich im Nahkampf!!
-
-
+    //Ladebildschirm mit Katharina
     
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    //cheats
