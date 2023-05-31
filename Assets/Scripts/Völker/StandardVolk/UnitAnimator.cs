c4924e71630e9d743a788e70474a009fe2ee5aa2
0xKatharina <130848315+0xKatharina@users.noreply.github.com>
2023-05-31 14:37:10 +0200

    paar Arrays für die Animation fertig gemacht

--- Assets/Scripts/Völker/StandardVolk/UnitAnimator.cs
+++ Assets/Scripts/Völker/StandardVolk/UnitAnimator.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 public class UnitAnimator : MonoBehaviour
 {
     public Sprite[] idle;
-    public Sprite[] moveForwardL;
-    //public Sprite[] moveForwardR;
-    public Sprite[] moveBackL;
-    //public Sprite[] moveBackR;
+    public Sprite[] moveForwardBLUE;
+    public Sprite[] moveBackBLUE;
+    public Sprite[] moveForwardRED;
+    public Sprite[] moveBackRED;
+    public Sprite[] moveForwardGREEN;
+    public Sprite[] moveBackGREEN;
+    public Sprite[] moveForwardPURP;
+    public Sprite[] moveBackPURP;
 
     private SpriteRenderer spriteRenderer; 
+    private Sprite[] forward;
+    private Sprite[] back;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        int spielerFarbe = GetComponent<UnitSprite>().id;
+        Debug.Log(spielerFarbe);
+
+        if(spielerFarbe == 1) {        //BLAU
+            forward = moveForwardBLUE;
+            back = moveBackBLUE;
+        } else if(spielerFarbe == 2) { //ROT
+            forward = moveForwardRED;
+            back = moveBackRED;
+        } else if(spielerFarbe == 3) { //GRÜN
+            forward = moveForwardGREEN;
+            back = moveBackGREEN;
+        } else {                       //LILA
+            forward = moveForwardPURP;
+            back = moveBackPURP;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        int index = Mathf.FloorToInt(Time.time * 4) % moveForwardL.Length;
-        spriteRenderer.sprite = moveForwardL[index];
+
+        int index = Mathf.FloorToInt(Time.time * 4) % 4; //erste 4 für Geschwindigkeit, zweite 4 weil in jedem Array 4 Sprites drin sind
+        Debug.Log(index);
+        spriteRenderer.sprite = forward[index];
     }
 
+
+    //Sprite rumdrehen
     void rumdrehen() {
         GetComponent<Transform>().Rotate(new Vector3(0f, 180f, 0f));
     }
